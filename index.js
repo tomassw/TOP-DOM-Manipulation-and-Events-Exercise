320a5d782cb177ca578dc49a4bef24e8031cbfbf
tomassw <75063636+tomassw@users.noreply.github.com>
2022-11-05 13:24:30 +0200

    Added the p and h3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("All set up");
+const container = document.querySelector('.container');
+
+
+//adding p
+const p = document.createElement('p');
+p.classList.add('red');
+p.textContent = 'Hey, I\'m red';
+
+container.appendChild(p);
+
+p.style.cssText = 'color: red; font-size: 18px;';
+
+
+//adding h3
+const h3 = document.createElement('h3');
+h3.textContent = "I'm a blue h3";
+h3.style.cssText = 'color: blue; font-size: 18px;';
+
+container.appendChild(h3);
+
+//addinv a div
+const secdiv = document.createElement('div');
+
